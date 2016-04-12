6c449cf6606b3c147a9c9d1ad7511be36c3cdaca
Antonio Amiel L. Yap <ahmilix.yap@gmail.com>
2016-04-12 11:19:02 +0800

    Removed entities for now

--- Tudskee.Data/Infrastructure/HibernateSessionFactory.cs
+++ Tudskee.Data/Infrastructure/HibernateSessionFactory.cs
@@ -1,6 +1,9 @@
-﻿using FluentNHibernate.Cfg;
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions.Helpers;
 using NHibernate;
+using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
 using System;
 using System.Collections.Generic;
@@ -24,9 +27,9 @@ namespace Tudskee.Data.Infrastructure
                 if (_sessionFactory == null)
                 {
                     _sessionFactory = Fluently.Configure()
-                          .Database(MySQLConfiguration.Standard.ConnectionString("tudskee_connection"))
-                          .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
-                          .ExposeConfiguration(config => new SchemaUpdate(config).Execute(false, true))
+                          .Database(MySQLConfiguration.Standard.ConnectionString(c => c.FromConnectionStringWithKey("tudskee_connection")))
+                          .Mappings(m => m.AutoMappings.Add(CreateMappings()))
+                          .ExposeConfiguration(UpdateSchema)
                           .BuildSessionFactory();
                 }
             }
@@ -39,14 +42,36 @@ namespace Tudskee.Data.Infrastructure
                 if (_sessionFactory == null)
                 {
                     _sessionFactory = Fluently.Configure()
-                          .Database(MySQLConfiguration.Standard.ConnectionString("tudskee_connection"))
-                          .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
-                          .ExposeConfiguration(config => new SchemaUpdate(config).Execute(false, true))
+                          .Database(MySQLConfiguration.Standard.ConnectionString(c => c.FromConnectionStringWithKey("tudskee_connection")))
+                          .Mappings(m => m.AutoMappings.Add(CreateMappings()))
+                          .ExposeConfiguration(UpdateSchema)
                           .BuildSessionFactory();
                 }
             }
 
             return _sessionFactory.OpenSession();
+        }
+
+        // Returns our mappings
+        private static AutoPersistenceModel CreateMappings()
+        {
+            return AutoMap
+                .Assembly(System.Reflection.Assembly.GetCallingAssembly())
+                .Where(t => t.Namespace == "Tudskee.Entities")
+                .Conventions.Setup(c => c.Add(DefaultCascade.SaveUpdate()));
+        }
+
+        // Drops and creates the database schema
+        private static void DropCreateSchema(Configuration cfg)
+        {
+            new SchemaExport(cfg)
+                .Create(false, true);
+        }
+
+        // Updates the database schema if there are any changes to the model
+        private static void UpdateSchema(Configuration cfg)
+        {
+            new SchemaUpdate(cfg);
         }
     }
 }
